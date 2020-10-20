79be5d71cb10d0b85f0c485ba6302f5864b7d83f
Asidipta Chaudhuri <asidiptachaudhuri@gmail.com>
2020-10-20 16:38:36 +0530

    Updates api url

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: "http://localhost:8000"
-    baseURL: "https://sports-events-app.herokuapp.com/"
+    baseURL: "https://sports-events-app-backend.herokuapp.com"
 });
 
 export default api;
